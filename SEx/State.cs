978d6a9c319bac70955caeb65fb292c4d931d330
Roman Ustinov <romanustinov9@gmail.com>
2019-01-23 00:10:03 +0300

    Correct equality tests

--- SEx/State.cs
+++ SEx/State.cs
@@ -6,25 +6,28 @@ using System.Threading.Tasks;
 
 namespace SEx
 {
-    struct State
+    struct State : IEquatable<State>
     {
         public int count;
         public int @switch;
 
         public override bool Equals(object obj)
         {
-            if (!(obj is State))
-            {
-                return false;
-            }
+            return obj is State && Equals((State)obj);
+        }
 
-            var state = (State)obj;
-            return count == state.count;
+        public bool Equals(State other)
+        {
+            return count == other.count &&
+                   @switch == other.@switch;
         }
 
         public override int GetHashCode()
         {
-            return 1110609940 + count.GetHashCode();
+            var hashCode = -703402107;
+            hashCode = hashCode * -1521134295 + count.GetHashCode();
+            hashCode = hashCode * -1521134295 + @switch.GetHashCode();
+            return hashCode;
         }
 
         public static bool operator ==(State state1, State state2)
