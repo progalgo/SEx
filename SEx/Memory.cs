bac27678af2ae390afc82afd450f4589ac07a896
Roman Ustinov <romanustinov9@gmail.com>
2021-10-05 13:45:25 +0300

    Add memory type

---
+++ SEx/Memory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEx
+{
+    class Memory
+    {
+        readonly byte[] bytes;
+
+        public Memory(uint size)
+        {
+            bytes = new byte[size];
+        }
+
+        public byte? Read(uint address)
+        {
+            return address < bytes.Length ? bytes[address] : (byte?)null;
+        }
+    }
+}
